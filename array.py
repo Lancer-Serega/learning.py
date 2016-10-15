639e2d8e0f3c6e19965c29945e616f97ad3cb689
Lancer-Serega <lancer-serega@mail.ru>
2016-10-15 06:56:09 +0300

    + dict (array)

--- array.py
+++ array.py
@@ -1,19 +1,22 @@
 phoneBook = dict(
-    franc="Contact \"franc\"",
-    anna="Contact \"anna\"",
-    andrey="Contact \"andrey\"",
-    kostik="Contact \"kostik\"",
-    masha="Contact \"masha\"",
-    lisa="Contact \"lisa\"",
-    dima="Contact \"dima\"",
-    ira="Contact \"ira\"",
+    franc="- \"franc\"",
+    anna="- \"anna\"",
+    andrey="- \"andrey\"",
+    kostik="- \"kostik\"",
+    masha="- \"masha\"",
+    lisa="- \"lisa\"",
+    dima="- \"dima\"",
+    ira="- \"ira\"",
 )
 
 post = str(input("Кого ищем? : "))
 
+# result = phoneBook.get(post, 'Контакт "' + post + '" не найден!')
+# print(result)
+
 if post in phoneBook:
-    print("Контакт найден! ")
+    print('Контакт "' + post + '" найден! ')
     print(phoneBook[post])
 else:
-    print("Контакт к сожалению не найден!")
-    print("Попробуйте изменить параметры поиска")
+    print('Контакт к "' + post + '" сожалению не найден!')
+    print('Попробуйте изменить параметры поиска')
